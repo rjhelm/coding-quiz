20bbcce2e0a1db2c393ded85274224dc4ccd4c6f
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-02-21 23:12:52 -0700

    alot of debugging and changing wording-also added styles

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,237 +1,229 @@
-// Javascript Quiz Code 
 
-// Questions array for questions user will be asked
-let questions = [
+// Assignment Javascript Code
+// Questions and answers array for the quiz
+var quizQuestions = [
   {
-    askUser: "Which of the following is not Javascript frameworks or libraries?",
-    userChoice: ["1. Polymer", "2. Meteor", "3. Cassandra", "4. jQuery"],
-    correctAnswer: "Cassandra",
+    question:
+      "Which of the following is not Javascript frameworks or libraries?",
+    choiceA: "Polymer",
+    choiceB: "Meteor",
+    choiceC: "Cassandra",
+    choiceD: "jQuery",
+    correctAnswer: "c",
   },
   {
-    askUser:
-      "Among the following, which one is a ternary operator in Javascript?",
-    userChoice: ["1. #", "2. ::", "3. &", "4. ?"],
-    correctAnswer: "?",
+    question: "Among the following, which one is a ternary operator?",
+    choiceA: "#",
+    choiceB: "::",
+    choiceC: "&",
+    choiceD: "?",
+    correctAnswer: "d",
   },
   {
-    askUser: "What are the two basic groups of dataypes in JavaScript?",
-    userChoice: [
-      "1. Primitive",
-      "2. Reference types",
-      "3. All of the above",
-      "4. None of the above",
-    ],
-    correctAnswer: "All of the above",
+    question: "What are the two basic groups of datatypes in Javascript?",
+    choiceA: "Primitive",
+    choiceB: "Reference Types",
+    choiceC: "All of the above",
+    choiceD: "None of the above",
+    correctAnswer: "c",
   },
   {
-    askUser:
-      "Which of the following method checks if its argument is not a number?",
-    userChoice: [
-      "1. isNan()",
-      "2. nonNaN()",
-      "3. NaN()",
-      "4. None of the above",
-    ],
-    correctAnswer: "isNaN()",
+    question:
+      "Which of the following methods checks if its argument is not a number?",
+    choiceA: "isNaN()",
+    choiceB: "nonNaN()",
+    choiceC: "NaN()",
+    choiceD: "None of the above",
+    correctAnswer: "a",
   },
   {
-    askUser: "What is the purpose of the Attr object in the HTML DOM?",
-    userChoice: [
-      "1. Used to focus on a particular part of the HTML page",
-      "2. HTML Attribute",
-      "3. Used to arrange elements",
-      "4. Not mentioned",
-    ],
-    correctAnswer: "HTML Attribute",
+    question: "What is the purpose of the Attr object in HTML DOM?",
+    choiceA: "Used to focus on a particular part of the HTML page",
+    choiceB: "HTML Attribute",
+    choiceC: "Used to arrange elements",
+    choiceD: "Not mentioned",
+    correctAnswer: "b",
   },
   {
-    askUser: "JavaScript can be written __________",
-    userChoice: [
-      "1. directly into HTML pages",
-      "2. directly on the server page",
-      "3. directly into css file",
-      "4. directly into JS file and included in the HTML file",
-    ],
-    correctAnswer: "directly into JS file and included in the HTML file",
+    question: "JavaScript can be written __________?",
+    choiceA: "directly into HTML pages",
+    choiceB: "directly on the server page",
+    choiceC: "into a css file",
+    choiceD: "directly into JS file and included in HTML file",
+    correctAnswer: "d",
+  },
+  {
+    question: "With which of the following do you wrap an array?",
+    choiceA: "[]",
+    choiceB: "<>",
+    choiceC: "()",
+    choiceD: "{}",
+    correctAnswer: "a",
   },
 ];
 
-// Start time for quiz timer
-let timeLeft = 75;
+// HTML elements as global variables to plug in generated information
+let quizBody = document.getElementById("quiz-body");
+let choiceResult = document.getElementById("choice-result");
+let startContainer = document.getElementById("start-container");
+let quizTimer = document.getElementById("timer");
+let startQuizButton = document.getElementById("start-btn");
+let userScore = document.getElementById("user-score");
+let quizComplete = document.getElementById("quiz-complete");
+let scoreContainer = document.getElementById("score-container");
+let questionsElement = document.getElementById("questions");
+let buttonA = document.getElementById("a");
+let buttonB = document.getElementById("b");
+let buttonC = document.getElementById("c");
+let buttonD = document.getElementById("d");
+let scoresPage = document.getElementById("scores-page");
+let userInitials = document.getElementById("initials");
+let scoreInitials = document.getElementById("score-initials");
+let backClearBtns = document.getElementById("back-clear");
+let saveScoreBtn = document.getElementById("save-score");
+let userSavedScores = document.getElementById("user-saved");
 
-let timer = document.getElementById("timer");
+// Global Variables to be called on 
+var questionIndex = quizQuestions.length;
+var questionsCurrent = 0;
+var timeLeft = 76;
+var timerInterval;
+var score = 0;
+var correct;
 
-let scoresContainer = document.getElementById("scores-container");
-// Reference for created buttons to placed in
-let pageButtons = document.getElementById("buttons");
-// Reference for score information
-let viewScoresBtn = document.getElementById("view-scores");
-
-// Start the quiz 
-let startQuiz = document.getElementById("start-quiz");
-startQuiz.addEventListener("click", quizTime);
-
-// Quiz questions, multiple choices, and answer result(correct or wrong)
-let quizQuestions = document.getElementById("quiz-questions");
-
-let quizResults = document.getElementById("quiz-results");
-
-let quizChoice = document.getElementById("quiz-choice");
-
-// Variables for tracking user input information
-let emptyScore = [];
-
-let userScores = JSON.parse(window.localStorage.getItem("userScores"));
-
-let currentQuestion = 0;
-
-let score = 0;
-
-// Timer start function
-function quizTime() {
-  generateQuestions();
-  let defineTimer = setInterval(function () {
-    timeLeft--;
-    timer.textContent = "";
-    timer.textContent = "Time Remaining: " + timeLeft;
-    if (timeLeft <= 0 || currentQuestion === questions.length) {
-      clearInterval(defineTimer);
-      generateScore();
-    }
-  }, 1000);
-}
-
-// Once quiz is started pull the information from the array above
-// Create the Question Dialog and buttons to select a choice
-function generateQuestions() {
-  removeElement(startQuiz);
-
-  if (currentQuestion < questions.length) {
-    quizQuestions.innerHTML = questions[currentQuestion].askUser;
-    quizChoice.textContent = "";
-
-    for (let i = 0; i < questions[currentQuestion].userChoice.length; i++) {
-      let quizBtn = document.createElement("button");
-      quizBtn.innerText = questions[currentQuestion].userChoice[i];
-      quizBtn.setAttribute("data-id", i);
-      quizBtn.addEventListener("click", function (event) {
-        event.stopPropagation();
-
-        if (quizBtn.innerText === questions[currentQuestion].correctAnswer) {
-          score += timeLeft;
-        } else {
-          score -= 10;
-          timeLeft = timeLeft - 15;
-        }
-
-        quizQuestions.innerHTML = "";
-
-        if (currentQuestion === questions.length) {
-          return;
-        } else {
-          currentQuestion++;
-          generateQuestions();
-        }
-      });
-      quizChoice.append(quizBtn);
-    }
+// Start the quiz and produce question
+function generateQuizQuestion() {
+  quizComplete.style.display = "none";
+  if (questionsCurrent === questionIndex) {
+    return showScore();
   }
+  var currentQuestion = quizQuestions[questionsCurrent];
+  questionsElement.innerHTML = "<p>" + currentQuestion.question + "</p>";
+  buttonA.innerHTML = currentQuestion.choiceA;
+  buttonB.innerHTML = currentQuestion.choiceB;
+  buttonC.innerHTML = currentQuestion.choiceC;
+  buttonD.innerHTML = currentQuestion.choiceD;
 }
 
-// Generate the score after quiz is complete and then gives user option to save
-function generateScore() {
-  timer.remove();
-  quizChoice.textContent = "";
 
-  let userInitials = document.createElement("input");
-  let saveScoreBtn = document.createElement("input");
+function startQuiz() {
+  quizComplete.style.display = "none";
+  startContainer.style.display = "none";
+  generateQuizQuestion();
 
-  quizResults.innerHTML = `You scored ${score} points! Enter initials: `;
-  userInitials.setAttribute("type", "text");
-  saveScoreBtn.setAttribute("type", "button");
-  saveScoreBtn.setAttribute("value", "Post My Score!");
-  saveScoreBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    let savedScores = isLocalScores(userScores, emptyScore);
+  //Timer
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    quizTimer.textContent = "Time left: " + timeLeft;
 
-    let initials = userInitials.value;
-    let initialsScore = {
-      initials: initials,
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      showScore();
+    }
+  }, 500);
+  quizBody.style.display = "block";
+}
+// This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
+function showScore() {
+  quizBody.style.display = "none";
+  quizComplete.style.display = "flex";
+  clearInterval(timerInterval);
+  userInitials.value = "";
+  userScore.innerHTML =
+    "You got " + score + " points!";
+}
+
+// User is able to save the score to local storage to be viewed later
+saveScoreBtn.addEventListener("click", function showScore() {
+  if (userInitials.value === "") {
+    alert("This cannot be blank!");
+    return false;
+  } else {
+    var prevUserScores =
+      JSON.parse(localStorage.getItem("prevUserScores")) || [];
+    var currentUser = userInitials.value.trim();
+    var gameScore = {
+      name: currentUser,
       score: score,
     };
 
-    savedScores.push(initialsScore);
-    localScores(savedScores);
-    userViewScores();
-    userClearBtn();
-    returnToQuiz();
-    viewScoresBtn.remove();
-  });
-  quizResults.append(userInitials);
-  quizResults.append(saveScoreBtn);
-}
+    quizComplete.style.display = "none";
+    scoreContainer.style.display = "flex";
+    scoresPage.style.display = "block";
+    backClearBtns.style.display = "flex";
 
-const removeElement = (...els) => {
-  for (let el of els) el.remove();
-};
-
-const isLocalScores = (arr1, arr2) => {
-  if (arr1 !== null) {
-    return arr1;
-  } else {
-    return arr2;
+    prevUserScores.push(gameScore);
+    localStorage.setItem("prevUserScores", JSON.stringify(prevUserScores));
+    generateUserScores();
   }
-};
+});
 
-const localScores = (array) => {
-  window.localStorage.setItem("userScores", JSON.stringify(array));
-};
-
-function userViewScores() {
-  removeElement(timer, startQuiz, quizResults);
-  let savedScores = isLocalScores(userScores, emptyScore);
-
-  savedScores.forEach((obj) => {
-    let initials = obj.initials;
-    let userScores = obj.score;
-    let quizResultsP = document.createElement("p");
-    quizResultsP.innerText = `${initials}: ${userScores}`;
-    scoresContainer.append(quizResultsP);
-  });
+// Create a list from local storage with user saved scores
+function generateUserScores() {
+  scoreInitials.innerHTML = "";
+  userSavedScores.innerHTML = "";
+  var highScores = JSON.parse(localStorage.getItem("prevUserScores")) || [];
+  for (i = 0; i < highScores.length; i++) {
+    var enterName = document.createElement("li");
+    var userNewScore = document.createElement("li");
+    enterName.textContent = highScores[i].name;
+    userNewScore.textContent = highScores[i].score;
+    scoreInitials.appendChild(enterName);
+    userSavedScores.appendChild(userNewScore);
+  }
 }
 
-function viewScores() {
-  viewScoresBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    removeElement(timer, startQuiz);
-    userViewScores();
-    removeElement(viewScoresBtn);
-    userClearBtn();
-    returnToQuiz();
-  });
+
+function showScores() {
+  startContainer.style.display = "none";
+  quizComplete.style.display = "none";
+  scoreContainer.style.display = "flex";
+  scoresPage.style.display = "block";
+  backClearBtns.style.display = "flex";
+
+  generateUserScores();
 }
 
-function userClearBtn() {
-  let clearBtn = document.createElement("input");
-  clearBtn.setAttribute("type", "button");
-  clearBtn.setAttribute("value", "Clear Your Scores");
-  clearBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    removeElement(scoresContainer);
-    window.localStorage.removeItem("userScores");
-  });
-  scoresContainer.append(clearBtn);
+// User is able to clear the local storage and start fresh
+function clearScore() {
+  window.localStorage.clear();
+  scoreInitials.textContent = "";
+  userSavedScores.textContent = "";
 }
 
-function returnToQuiz() {
-  let toQuizBtn = document.createElement("input");
-  toQuizBtn.setAttribute("type", "button");
-  toQuizBtn.setAttribute("value", "Return To Quiz");
-  toQuizBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.reload();
-  });
-  pageButtons.append(toQuizBtn);
+// User may go back to the quiz and try for better score
+function tryAgain() {
+  scoreContainer.style.display = "none";
+  quizComplete.style.display = "none";
+  startContainer.style.display = "flex";
+  timeLeft = 76;
+  score = 0;
+  questionsCurrent = 0;
 }
 
-viewScores();
+// This allows us to check each answer and subtract time when incorrect or let them know they were right
+function checkAnswer(answer) {
+  correct = quizQuestions[questionsCurrent].correctAnswer;
+
+  if (answer === correct && questionsCurrent !== questionIndex) {
+    score+= timeLeft;
+    alert("That Is Correct!");
+    questionsCurrent++;
+    generateQuizQuestion();
+    //display in the results div that the answer is correct.
+  } else if (
+    answer !== correct &&
+    questionsCurrent !== questionIndex
+  ) {
+    alert("That Is Incorrect.");
+    timeLeft = timeLeft - 15;
+    questionsCurrent++;
+    generateQuizQuestion();
+    //display in the results div that the answer is wrong.
+  } else {
+    showScore();
+  }
+
+}
+
+startQuizButton.addEventListener("click", startQuiz);
